341010351ca8ba00692f9d04c30afd44fc5872ad
kentlin0919 <kent900919@gmail.com>
2023-07-05 14:54:02 +0800

    feat: App框架新增

---
+++ lib/Passerby_Mode/MassTransit_Mode.dart
@@ -0,0 +1,63 @@
+// ignore_for_file: file_names, camel_case_types, prefer_typing_uninitialized_variables
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+//引入頁面
+import '../App_Page/Home_Page.dart';
+import '../App_Page/News_Page.dart';
+
+
+
+
+//主程式
+class MassTransitMode extends StatefulWidget {
+  const MassTransitMode({super.key});
+
+  @override
+  State<MassTransitMode> createState() => _MassTransitMode();
+}
+
+class _MassTransitMode extends State<MassTransitMode> {
+
+  final List<BottomNavigationBarItem> bottonTabs = [
+    const BottomNavigationBarItem(
+        label: '大眾運輸首頁', icon: Icon(CupertinoIcons.home), tooltip: "大眾運輸首頁"),
+    const BottomNavigationBarItem(
+        label: '最新消息', icon: Icon(CupertinoIcons.news_solid), tooltip: "最新消息"),
+
+  ];
+
+  final List tabBodies = [
+    const Home(),
+    const News(),
+
+  ];
+
+//將預設標籤設定為0
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottonTabs,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
+      ),
+      body: currentPage,
+      backgroundColor: const Color.fromRGBO(255, 255, 255, 0.498),
+    );
+  }
+}
