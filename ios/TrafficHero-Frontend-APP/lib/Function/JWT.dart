f56a0fa1b5a795c1318fbac5f3b427a5af9badb7
kentlin0919 <96437895+kentlin0919@users.noreply.github.com>
2023-08-01 12:48:34 +0800

    fix

--- ios/TrafficHero-Frontend-APP/lib/Function/JWT.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: camel_case_types, avoid_print, non_constant_identifier_names, void_checks, file_names
-import 'package:traffic_hero/imports.dart';
-
-class jwt {
-  JWT jwtdecode (token){
-     
-    return JWT.decode(token);
-  }
-}
