9c49d5033f0aed6e0ad489b5cc9d86e77f485b19
kentlin0919 <96437895+kentlin0919@users.noreply.github.com>
2023-08-02 11:53:34 +0800

    fix

---
+++ ios/TrafficHero-Frontend-APP/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,56 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<firebase_auth/FLTFirebaseAuthPlugin.h>)
+#import <firebase_auth/FLTFirebaseAuthPlugin.h>
+#else
+@import firebase_auth;
+#endif
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<geocoding_ios/GeocodingPlugin.h>)
+#import <geocoding_ios/GeocodingPlugin.h>
+#else
+@import geocoding_ios;
+#endif
+
+#if __has_include(<google_sign_in_ios/FLTGoogleSignInPlugin.h>)
+#import <google_sign_in_ios/FLTGoogleSignInPlugin.h>
+#else
+@import google_sign_in_ios;
+#endif
+
+#if __has_include(<libphonenumber_plugin/FLTLibphonenumberPlugin.h>)
+#import <libphonenumber_plugin/FLTLibphonenumberPlugin.h>
+#else
+@import libphonenumber_plugin;
+#endif
+
+#if __has_include(<url_launcher_ios/FLTURLLauncherPlugin.h>)
+#import <url_launcher_ios/FLTURLLauncherPlugin.h>
+#else
+@import url_launcher_ios;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseAuthPlugin"]];
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [GeocodingPlugin registerWithRegistrar:[registry registrarForPlugin:@"GeocodingPlugin"]];
+  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
+  [FLTLibphonenumberPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTLibphonenumberPlugin"]];
+  [FLTURLLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTURLLauncherPlugin"]];
+}
+
+@end
--- lib/App_Page/App_Function_Page/News_Page.dart
+++ lib/App_Page/App_Function_Page/News_Page.dart
@@ -20,11 +20,10 @@ class _NewsState extends State<News> {
     super.didChangeDependencies();
     EasyLoading.show(status: 'Loading.....');
     state = Provider.of<stateManager>(context, listen: false);
-  get_News();
+    get_News();
 
     //依照模式判斷顯示內容
     if (state.modeName == 'car') {
-
       setState(() {
         print(listView);
         List_City = choices.city;
@@ -72,68 +71,17 @@ class _NewsState extends State<News> {
     var jwt = state.accountState;
     try {
       response = await api().api_Get(url, jwt);
-       setState(() {
-        listView =  jsonDecode(utf8.decode(response.bodyBytes));
-
+      setState(() {
+        listView = jsonDecode(utf8.decode(response.bodyBytes));
       });
     } catch (e) {
       EasyLoading.showError(e.toString());
     }
 
     if (response.statusCode == 200) {
-        listView =  jsonDecode(utf8.decode(response.bodyBytes));
-        EasyLoading.dismiss();
-
+      listView = jsonDecode(utf8.decode(response.bodyBytes));
+      EasyLoading.dismiss();
     }
-  }
-
-  Widget dropdown_select(
-      select_name, select_name_English, select_Value, list_value) {
-    return SmartSelect<String>.multiple(
-        title: select_name,
-        placeholder: select_name_English,
-        selectedValue: select_Value,
-        onChange: (selected) {
-          setState(() =>
-              {select_Value = selected.value,    EasyLoading.show(status: 'Loading.....'), get_News(), print(listView)});
-        },
-        choiceItems: S2Choice.listFrom<String, Map<String, String>>(
-          source: list_value,
-          value: (index, item) => item['value'] ?? '',
-          title: (index, item) => item['title'] ?? '',
-          group: (index, item) => item['body'] ?? '',
-        ),
-        choiceActiveStyle: const S2ChoiceStyle(color: Colors.redAccent),
-        modalType: S2ModalType.bottomSheet,
-        modalConfirm: true,
-        modalFilter: true,
-        groupEnabled: true,
-        groupSortBy: S2GroupSort.byCountInDesc(),
-        groupBuilder: (context, state, group) {
-          return StickyHeader(
-            header: state.groupHeader(group),
-            content: state.groupChoices(group),
-          );
-        },
-        groupHeaderBuilder: (context, state, group) {
-          return Container(
-            color: primaryColor,
-            padding: const EdgeInsets.all(15),
-            alignment: Alignment.centerLeft,
-            child: S2Text(
-              text: group.name,
-              highlight: state.filter?.value,
-              highlightColor: Colors.teal,
-              style: const TextStyle(color: Colors.white),
-            ),
-          );
-        },
-        tileBuilder: (context, state) {
-          return S2Tile.fromState(
-            state,
-            isTwoLine: true,
-          );
-        });
   }
 
   @override
@@ -183,7 +131,7 @@ class _NewsState extends State<News> {
                             )),
                             onTap: () {
                               get_News();
-                                  EasyLoading.show(status: 'Loading.....');
+                              EasyLoading.show(status: 'Loading.....');
                               setState(() {
                                 Select_1 = [];
                                 Select_2 = [];
@@ -273,5 +221,58 @@ class _NewsState extends State<News> {
             )),
       ),
     );
+  }
+
+  Widget dropdown_select(
+      select_name, select_name_English, select_Value, list_value) {
+    return SmartSelect<String>.multiple(
+        title: select_name,
+        placeholder: select_name_English,
+        selectedValue: select_Value,
+        onChange: (selected) {
+          setState(() => {
+                select_Value = selected.value,
+                EasyLoading.show(status: 'Loading.....'),
+                get_News(),
+                print(listView)
+              });
+        },
+        choiceItems: S2Choice.listFrom<String, Map<String, String>>(
+          source: list_value,
+          value: (index, item) => item['value'] ?? '',
+          title: (index, item) => item['title'] ?? '',
+          group: (index, item) => item['body'] ?? '',
+        ),
+        choiceActiveStyle: const S2ChoiceStyle(color: Colors.redAccent),
+        modalType: S2ModalType.bottomSheet,
+        modalConfirm: true,
+        modalFilter: true,
+        groupEnabled: true,
+        groupSortBy: S2GroupSort.byCountInDesc(),
+        groupBuilder: (context, state, group) {
+          return StickyHeader(
+            header: state.groupHeader(group),
+            content: state.groupChoices(group),
+          );
+        },
+        groupHeaderBuilder: (context, state, group) {
+          return Container(
+            color: primaryColor,
+            padding: const EdgeInsets.all(15),
+            alignment: Alignment.centerLeft,
+            child: S2Text(
+              text: group.name,
+              highlight: state.filter?.value,
+              highlightColor: Colors.teal,
+              style: const TextStyle(color: Colors.white),
+            ),
+          );
+        },
+        tileBuilder: (context, state) {
+          return S2Tile.fromState(
+            state,
+            isTwoLine: true,
+          );
+        });
   }
 }
