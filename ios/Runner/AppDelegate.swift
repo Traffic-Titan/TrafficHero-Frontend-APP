f355255e51efec2ffac3ff8feafd79b78d477503
ting <lavender55260@gmail.com>
2023-10-08 21:57:31 +0800

    feat:Tourist Page Google Map OK、上拉式抽屜 OK(new)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-  GMSServices.provideAPIKey("AIzaSyB-CmWr48J9A1DpIFJafSjW5dmajFqV8EM")
+   GMSServices.provideAPIKey(API_KEY)
+//   GMSServices.provideAPIKey("AIzaSyB-CmWr48J9A1DpIFJafSjW5dmajFqV8EM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
