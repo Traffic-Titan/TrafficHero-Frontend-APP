2d9fcad9055f29f28dbcd29e94a7658cfe6fe850
ting <lavender55260@gmail.com>
2023-10-20 14:36:16 +0800

    feat:附近站點內容版面配置timer

---
+++ lib/App_Page/App_Function_Page/Public_Transport_Information_Page/Public_Transport_Information_Train.dart
@@ -0,0 +1,12 @@
+
+import 'package:traffic_hero/Imports.dart';
+
+//台鐵頁面
+Widget publicTransportInfoTrain(context){
+  var state = Provider.of<stateManager>(context, listen: false);
+  var  screenWidth = MediaQuery. of(context). size. width ;
+  return Text('台鐵');
+}
+
+
+
--- lib/App_Page/App_Function_Page/Tourist_Information_Page.dart
+++ lib/App_Page/App_Function_Page/Tourist_Information_Page.dart
@@ -18,7 +18,6 @@ class _Tourist_InformationState extends State<Tourist_Information> with TickerPr
   late stateManager state;
   var screenWidth;
   var screenHeight;
-  var positionNow;
   String searchText = '';
   late var tourismList = [];
   late GoogleMapController mapController;
@@ -32,7 +31,6 @@ class _Tourist_InformationState extends State<Tourist_Information> with TickerPr
   void didChangeDependencies() async {
     super.didChangeDependencies();
     state = Provider.of<stateManager>(context, listen: false);
-    positionNow = state.positionNow;
     screenWidth = MediaQuery. of(context). size. width ;
     screenHeight = MediaQuery. of(context). size. height;
     state.changePositionNow(await geolocator().updataPosition());
@@ -117,8 +115,6 @@ class _Tourist_InformationState extends State<Tourist_Information> with TickerPr
     // 添加標記
     for (int i=0;i <tourismList.length;i++) {
       var list = tourismList[i];
-      print('tourismList${list['名稱']}');
-      print('經緯度${list['經緯度'][0]}${list['經緯度'][1]}');
       _markers.add(
         Marker(
           markerId: MarkerId(list['名稱']),
@@ -126,7 +122,11 @@ class _Tourist_InformationState extends State<Tourist_Information> with TickerPr
           infoWindow: InfoWindow(
             title: list['名稱'],
             snippet: list['地址'],
+
           ),
+          onTap: (){
+            print('112');
+          }
         ),
       );
     }
