4c4302bd1f63fa4e670d134a1f627e7902b12c5b
ting <lavender55260@gmail.com>
2023-11-23 14:16:41 +0800

    style:高鐵車次查詢結果頁面修改版面、新增起迄站查詢的日期選擇限制

--- lib/App_Page/App_Function_Page/Public_Transport_Information_Page/THSR_CarNumSearchResult.dart
+++ lib/App_Page/App_Function_Page/Public_Transport_Information_Page/THSR_CarNumSearchResult.dart
@@ -27,40 +27,64 @@ class _THSR_CarNumSearchResultState extends State<THSR_CarNumSearchResult> {
     return Scaffold(
       backgroundColor: Color.fromRGBO(221, 235, 247, 1),
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Color.fromRGBO(113, 170, 221, 1),
+        iconTheme: IconThemeData(//返回鍵顏色
+          color:Colors.white, //change your color here
+        ),
+        titleTextStyle: TextStyle(color: Colors.white,fontSize: 20),
         title: Text("車次${carNum}"),
       ),
-      body: ListView.builder(
-          itemCount: carNumSearchDetail.length,
-          itemBuilder: (context, index){
-            var list = carNumSearchDetail[index];
-            return ListTile(
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Expanded(
-                    flex: 4,
-                    child: (index == carNumSearchDetail.length - 1)
-                        ?(Image.asset('assets/publicTransportIcon/routeDot.png', height: 35,))
-                        :(Image.asset(
-                      'assets/publicTransportIcon/routeLine.png',
-                      height: 60,
-                    )),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Text(list['停靠站']),
-                  ),
-                  Expanded(
-                      flex: 3,
-                      child: Text(list['預估抵達時間'])
-                  )
-                ],
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 3,
+                child: Text('')
               ),
-            );
-          }
-      ),
+              Expanded(
+                flex: 3,
+                child: Text('停靠站',style: TextStyle(fontSize: 20,color:  Color.fromRGBO(24, 60, 126, 1)),textAlign: TextAlign.center,)
+              ),
+              Expanded(
+                  flex: 4,
+                  child: Text('預估抵達時間',style: TextStyle(fontSize: 20,color:  Color.fromRGBO(24, 60, 126, 1)),textAlign: TextAlign.center,)
+              )
+            ],
+          ),
+          Flexible(
+              child: ListView.builder(
+                  itemCount: carNumSearchDetail.length,
+                  itemBuilder: (context, index){
+                    var list = carNumSearchDetail[index];
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: (index == 0)
+                              ?(Image.asset('assets/publicTransportIcon/routeDot.png', height: 35,))
+                              :(Image.asset('assets/publicTransportIcon/routeLine.png',height: 60,
+                          )),
+                        ),
+                        Expanded(
+                          flex: 3,
+                          child: Text(list['停靠站'],style: TextStyle(fontSize: 20,color:  Color.fromRGBO(24, 60, 126, 1)),textAlign: TextAlign.center,),
+                        ),
+                        Expanded(
+                            flex: 4,
+                            child: Text(list['預估抵達時間'],style: TextStyle(fontSize: 20,color:  Color.fromRGBO(24, 60, 126, 1)),textAlign: TextAlign.center,)
+                        )
+                      ],
+                    );
+                  }
+              ),
+          )
+        ],
+      )
     );
   }
 }
