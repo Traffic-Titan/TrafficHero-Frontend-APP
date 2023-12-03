f3ba36fe500b9f79490fdd3a99a92904d442a187
ting <lavender55260@gmail.com>
2023-12-03 13:22:16 +0800

    style:變更便利商店、加油站頁面

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: use_key_in_widget_constructors, library_private_types_in_public_api, prefer_const_constructors, unnecessary_new, avoid_print, use_build_context_synchronously
 
 
-import 'package:traffic_hero/firebase_options.dart';
+// import 'package:traffic_hero/firebase_options.dart';
 
 // import 'package:traffic_hero/firebase_options.dart';
 
@@ -15,9 +15,9 @@ void main() async {
   // await Firebase.initializeApp();
   try{
 
- await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+ // await Firebase.initializeApp(
+ //    options: DefaultFirebaseOptions.currentPlatform,
+ //  );
 
   await Firebase_message().initNotifications();
 
