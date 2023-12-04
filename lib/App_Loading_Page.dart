6b04a24318db0b8da21561be3faf4480eb6207fd
kentlin0919 <kent900919@gmail.com>
2023-12-04 12:47:25 +0800

    feat: 自動訂閱訊息

--- lib/App_Loading_Page.dart
+++ lib/App_Loading_Page.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: avoid_print, non_constant_identifier_names, prefer_typing_uninitialized_variables, file_names, use_build_context_synchronously, dead_code_catch_following_catch, unnecessary_null_comparison, sized_box_for_whitespace, prefer_interpolation_to_compose_strings, camel_case_types
 import 'package:traffic_hero/Imports.dart';
 
-
-
 class appLoadingPage extends StatefulWidget {
   const appLoadingPage({super.key});
 
@@ -36,7 +34,7 @@ class _appLoadingPage extends State<appLoadingPage> {
     }
     try {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
-      print('Running on ${iosInfo.utsname.machine}'); 
+      print('Running on ${iosInfo.utsname.machine}');
       if (iosInfo.utsname.machine != null) {
         prefs.setString('system', 'IOS');
       }
@@ -48,69 +46,64 @@ class _appLoadingPage extends State<appLoadingPage> {
 
   //執行token驗證並作出相對應的動作
   checkToken() async {
-     EasyLoading.show(status: '登入中...');
+    EasyLoading.show(status: '登入中...');
     var response;
     var url = dotenv.env['Profile'];
     var jwt = ',${prefs.get('userToken')}';
     print(jwt);
     try {
-       EasyLoading.show(status: '登入中...');
-      
+      EasyLoading.show(status: '登入中...');
+
       response = await api().apiGet(url, jwt);
       state.updateAccountState('${prefs.get('userToken')}');
 
-
-       if (response.statusCode == 200) {
-      
-      state.updateprofileState(jsonDecode(utf8.decode(response.bodyBytes)));
-      state.updateModeState(prefs.get('mode').toString());
-      Navigator.pushAndRemoveUntil(
-          context, MaterialPageRoute(builder: (context) => const AllPage()),(router)=>false);
-          EasyLoading.dismiss();
-    } else {
-       Navigator.pushAndRemoveUntil(
-          context, MaterialPageRoute(builder: (context) => const Login()),(router)=>false);
-          EasyLoading.dismiss();
-          EasyLoading.dismiss();
-    }
-
+      if (response.statusCode == 200) {
+        state.updateprofileState(jsonDecode(utf8.decode(response.bodyBytes)));
+        state.updateModeState(prefs.get('mode').toString());
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const AllPage()),
+            (router) => false);
+        EasyLoading.dismiss();
+      } else {
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const Login()),
+            (router) => false);
+        EasyLoading.dismiss();
+        EasyLoading.dismiss();
+      }
     } catch (e) {
       print(e);
       EasyLoading.dismiss();
     }
   }
 
-
 //app執行創建的頁面
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Color.fromRGBO(62, 111, 179, 1),
-        body: Center(child: 
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ClipRRect(
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(30.0),
                   topRight: Radius.circular(30.0),
                   bottomLeft: Radius.circular(30.0),
                   bottomRight: Radius.circular(30.0),
-                  
                 ),
-                child:   Image.asset('assets/AppIcon.png',
-                width: 200,
-                height: 200,),
-                  
-                
+                child: Image.asset(
+                  'assets/AppIcon.png',
+                  width: 200,
+                  height: 200,
+                ),
               ),
-          ],
-        ),
-        
-        
-      
-        )
-        );
+            ],
+          ),
+        ));
   }
 }
--- lib/Function/firebase_message.dart
+++ lib/Function/firebase_message.dart
@@ -1,22 +1,24 @@
 import '../Imports.dart';
 
+Future<void> handleBackgroundMessage(RemoteMessage message) async {
+  print('title:${message.notification?.title}');
+  print('body:${message.notification?.body}');
+  print('payload:${message.data}');
+}
 
-
-  Future<void> handleBackgroundMessage(RemoteMessage message) async {
-      print('title:${message.notification?.title}');
-       print('body:${message.notification?.body}');
-        print('payload:${message.data}');
-  }
-
-
-class Firebase_message{
+class Firebase_message {
   final _firebaseMess = FirebaseMessaging.instance;
+  List<dynamic> messageList = [];
+  late SharedPreferences prefs;
 
-
-
-  Future<void> initNotifications()async{
+  Future<void> initNotifications() async {
+    prefs = await SharedPreferences.getInstance();
     await _firebaseMess.requestPermission();
     final fCMToken = await _firebaseMess.getToken();
+    if (prefs.get('userToken') != '') {
+      await registerMessageToken(fCMToken);
+    }
+
     print('Token:$fCMToken');
     FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
@@ -25,11 +27,37 @@ class Firebase_message{
       print('body: ${message.notification?.body}');
       print('payload: ${message.data}');
     });
-        FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print('App opened from background message:');
       print('title: ${message.notification?.title}');
       print('body: ${message.notification?.body}');
       print('payload: ${message.data}');
+
+      messageList.add({
+        'title': message.notification?.title,
+        'body': message.notification?.body,
+        'payload': message.data
+      });
+
+      print(messageList);
     });
+  }
+
+  Future<void> registerMessageToken(token) async {
+    var response,
+        url = dotenv.env['Subscribe'].toString() + '?fcm_token=${token}',
+        jwt = ',${prefs.get('userToken')}',
+        Body;
+
+    try {
+      response = await api().apiPost(Body, url, jwt);
+      if (response.statusCode == 200) {
+        print(true);
+      } else {
+        print(response.statusCode);
+      }
+    } catch (e) {
+      print(e);
+    }
   }
 }
